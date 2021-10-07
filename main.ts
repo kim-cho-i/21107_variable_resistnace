82583351c4c6430639920bf7a8ed16e6ae1a8dff
kim-cho-i <90668331+kim-cho-i@users.noreply.github.com>
2021-10-07 12:30:09 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,21 @@
+function 전체끄기 () {
+	
+}
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    pins.analogWritePin(AnalogPin.P8, pins.analogReadPin(AnalogPin.P0))
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
